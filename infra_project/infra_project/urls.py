e96a7526cbb5d63f69dd1a90ea7fd05f853fc683
Семён Семёнов <semyon_semyonov_2003@bk.ru>
2023-05-28 14:17:29 +0400

    test5

--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
 
-
 urlpatterns = [
     path('', include('infra_app.urls', namespace='infra_app')),
     path('admin/', admin.site.urls),
